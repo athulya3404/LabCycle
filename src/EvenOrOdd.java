472e6b838031256f758d24d6db6b8a22743a8113
athulya3404 <athulya3404@gmail.com>
2026-06-27 19:24:20 +0530

    LabCycle1_11

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EvenOrOdd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        if (number % 2 == 0)
+        {
+            System.out.println(number + " is Even");
+        }
+        else
+        {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
